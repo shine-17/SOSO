bf739ec76a71de5bc8f09b80e0dccd3c08075c14
dohun <shine_17@naver.com>
2023-02-21 21:32:11 +0900

    SOSO init import

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <div id="app">
+  <div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="main_logo">SOSO</h1>
+    <div>
+      <h4>키보드</h4>
+      <p>{{ keyboard_price }}</p>
+    </div>
+    <div>
+      <h4>마우스</h4>
+      <p>{{ mouse_price }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      keyboard_price: "37,000",
+      mouse_price: "15,000"
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -24,5 +36,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.main_logo{
+  color: darkorange;
 }
 </style>
