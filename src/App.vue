438d3e3ce8cfe231125adf90299a8e72c12e61ab
dohun <shine_17@naver.com>
2023-02-23 23:46:08 +0900

    router setting

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
     <router-link to="/1"> 1페이지로 </router-link>
     <br>
     <router-link to="/2"> 2페이지로 </router-link>
+    <router-view/>
     <Footer></Footer>
   </div>
 </template>
@@ -19,6 +20,31 @@
 <script>
 import Header from './components/common/Header.vue'
 import Footer from './components/common/Footer.vue'
+
+/*
+  [App.vue 설명]
+  1. App.vue 는 Vue 애플리케이션의 최상위 컴포넌트입니다
+
+  2. template : 
+    - 화면 상에 표시할 요소 작성 실시
+    - 컴포넌트의 모든 마크업 구조와 디스플레이 로직 작성
+
+  3. script : 
+    - import 구문을 사용해 template에서 사용할 컴포넌트 불러온다
+    - export default 구문에서 해당 App 컴포넌트가 main.js 파일에서 불러와지도록 내용 작성 실시
+    - export default 구문에서 모듈의 함수, 객체, 변수 등을 다른 모듈에서 가져다 사용 할 수 있도록 내보냅니다
+
+  4. style : 
+    - 스타일 지정 실시
+
+  5. {{ message }} : 데이터 바인딩 시 사용
+
+  6. data : 컴포넌트 생성 시 초기 데이터 설정 (리턴 값 지정)
+
+  7. methods : 메소드 정의 실시   
+
+  8. router-view : 라우터에서 설정한 뷰 로드 실시
+*/
 
 export default {
   name: 'App',
--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
+// [애플리케이션 생성 실시]
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import Page1 from './pages/page-1.vue'
+import Page2 from './pages/page-2.vue'
+
+/*
+  [main.js 설명]
+  1. 애플리케이션 진입점으로 Vue 초기화, 폴더 , 전역, 컴포넌트 , 라이브러리 등록 수행 실시
+  2. import App 을 사용해 최상위 App.vue 컴포넌트 지정 실시
+  3. createApp mount 를 사용해 index.html 파일 div id 값 설정 및 렌더링 시작점 지정 실시
+  4. router : 라우터는 웹페이지 간의 이동하는 방법 및 경로를 설정해주는 파일입니다
+*/
 
 Vue.config.productionTip = false
 
+// [앱 생성 실시]
 new Vue({
   VueRouter,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app') // [main 아이디 : 렌더링 시작점] 
 
+// [라우터 사용 설정]
 Vue.use(VueRouter);
-import Page1 from './pages/page-1.vue'
-import Page2 from './pages/page-2.vue'
+
 
 const routes =[
   {
