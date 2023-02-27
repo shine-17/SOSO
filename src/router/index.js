e4da663f084a87eb3c7ae44a8ae5e7bac4afbd49
dohun <shine_17@naver.com>
2023-02-27 22:50:21 +0900

    indexjs router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+// [router > index.js 설명]
+// 1. router : 라우터는 웹 페이지 간의 이동하는 방법 및 경로를 설정해주는 파일입니다
+// 2. router 사용 시 App.js 파일에서는 <router-view /> 를 사용해서 로드합니다
+
 Vue.use(VueRouter)
 
 const routes = [
